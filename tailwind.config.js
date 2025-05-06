68ceb253c09902123e0f2e77e7a90921772bdd15
ho8ae <xogh2242@gmail.com>
2025-05-07 00:26:19 +0900

    chore: nativewind, expo 구성

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: '#6FCEFC',
+          DEFAULT: '#4EBBFF',
+          dark: '#3AABF0',
+        },
+        secondary: {
+          light: '#FFB067',
+          DEFAULT: '#FF9D47',
+          dark: '#F08C37',
+        },
+        success: {
+          light: '#85E0A3',
+          DEFAULT: '#5DC983',
+          dark: '#4AB86F',
+        },
+        warning: {
+          light: '#FFDC72',
+          DEFAULT: '#FFCF40',
+          dark: '#F0C030',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
